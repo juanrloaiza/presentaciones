aa7ded886d803bc789aed925315a378d82908441
Juan R. Loaiza <juanrloaiza@gmail.com>
2025-03-28 10:23:35 -0300

    SADAF-JeanNicod Workshop and others

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ markdown_file_folder=$(dirname "$markdown_filepath")
 cd "$markdown_file_folder"
 
 echo "Building slides..."
-pandoc $markdown_file_name -s -t beamer --pdf-engine=xelatex --template $TEMPLATE --slide-level=2 -o "${markdown_file_name/md/pdf}"
+pandoc "$markdown_file_name" -s -t beamer --pdf-engine=xelatex --template $TEMPLATE --slide-level=2 -o "${markdown_file_name/md/pdf}"
 
 echo "Building handout..."
-pandoc $markdown_file_name -s -t beamer --pdf-engine=xelatex --template $TEMPLATE --slide-level=2 -M handout=true -o "${markdown_file_name/md/handout.pdf}"
+pandoc "$markdown_file_name" -s -t beamer --pdf-engine=xelatex --template $TEMPLATE --slide-level=2 -M handout=true -o "${markdown_file_name/md/handout.pdf}"
